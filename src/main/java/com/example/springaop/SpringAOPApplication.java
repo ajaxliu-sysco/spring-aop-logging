7bc0bad0981cceb8e9d750ca15604edcea4ed393
aliu4884 <aliu4884@corp.sysco.com>
2024-07-11 14:13:57 +0800

    feature-2

--- src/main/java/com/example/springaop/SpringAOPApplication.java
+++ src/main/java/com/example/springaop/SpringAOPApplication.java
@@ -10,7 +10,6 @@ import java.util.ResourceBundle;
 @SpringBootApplication
 public class SpringAOPApplication {
 	//feature-2
-	//feature-1
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAOPApplication.class, args);
 		Locale french = new Locale("fr", "FR");
